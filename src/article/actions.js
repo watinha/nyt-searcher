89fb00003185c80125ff586fe2b5ddf83392aaf4
Willian Watanabe <watinha@gmail.com>
2019-10-03 15:03:41 -0300

    include an action to fetch articles using axios and NYT API

--- src/article/actions.js
+++ src/article/actions.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export class ArticleActions {
     static start_req () { return { type: 'ARTICLE_REQUEST_START'}; }
     static end_req () { return { type: 'ARTICLE_REQUEST_END'}; }
@@ -5,6 +7,17 @@ export class ArticleActions {
         return {
             type: 'ARTICLE_LOAD',
             items: items
+        };
+    }
+    static search (query) {
+        return async (dispatch) => {
+            dispatch(ArticleActions.start_req());
+			let response = await axios.get(
+				'https://api.nytimes.com/svc/search/v2/articlesearch.json?' +
+				`q=${query}&api-key=j2SELYz4iSc3GfFXVxPtCbR2YZ7QGrCU`),
+				json = JSON.parse(response);
+			dispatch(ArticleActions.load(json['response']['docs']));
+            dispatch(ArticleActions.end_req());
         };
     }
 };
